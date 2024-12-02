78cb955a65e398fdab89cfb20774c3304abc3192
garryvar <darisvet.sokolov@yandex.ru>
2024-12-02 10:22:07 +0300

    Менят скрипт

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 window.addEventListener("load", () => {
-  document.body.innerHTML = `<h1>home-work-module-6.git</h1`;
+  document.body.innerHTML = `
+  <header>
+    <h1>
+      home-work-module-6.git
+    </h1>
+  </header>
+  <main></main>
+  <footer></footer>`;
 });
